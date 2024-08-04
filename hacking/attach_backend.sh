f17798892d1774ae97e0e37644521d3911917466
Richard Metzler <richard.metzler@paadee.com>
2024-08-04 07:24:06 +0200

    attach with test

--- hacking/attach_backend.sh
+++ hacking/attach_backend.sh
@@ -1,8 +1,17 @@
 #!/bin/bash -exu
 
-SYMFONY_BIN=./symfony-cli
-PROXY_JSON=~/.symfony5/proxy.json
+SYMFONY_BIN="./symfony-cli"
+PROXY_JSON="$HOME/.symfony5/proxy.json"
+CURL="curl --proxy localhost:7080"
+
+$SYMFONY_BIN proxy:domain:attach test
 
 $SYMFONY_BIN proxy:backend:attach test --basepath /httpbin --backend https://httpbin.org/
+
 $SYMFONY_BIN proxy:status
+
 cat $PROXY_JSON
+
+$CURL http://test.wip
+
+$CURL http://test.wip/httpbin/get
